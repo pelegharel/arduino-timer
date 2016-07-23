12b4139758775e1021311d7a1e7f9917e19bedff
pelegharel <harel67@gmail.com>
2016-07-23 14:07:47 +0000

    Reformat code

--- include/states.hpp
+++ include/states.hpp
@@ -5,47 +5,45 @@
 #include <boost/optional.hpp>
 
 namespace arduinoTimer {
-    using namespace std;
-    enum class LightStates {on, off, flicker};
-    
-    struct LightState {
-        int light;
-        LightStates state;
-    };
-    
-    template<int lightNum, int stateNum>
-    class LightsStates{
-      using LightsState = std::array<bool,lightNum>;
-      using TimedState = std::tuple<std::chrono::nanoseconds, LightsState>;
-      std::array<boost::optional<TimedState>, stateNum> states;
-      
-    public:
-      auto& update(std::chrono::nanoseconds timePassed, const LightsState& state) {
-        auto& lastState = std::get<0>(states);
-        
-        if(!lastState || std::get<1>(*lastState) != state){
-            std::rotate(states.begin(), states.end() - 1, states.end());
-            std::get<0>(states) = std::make_tuple(timePassed, state);
-        } else {
-            auto& lastTime = std::get<0>(*lastState);
-            lastTime = timePassed + lastTime;
-        }
-        
-        return *this;
-      }
-      
-      auto begin() const {
-        return states.cbegin();
-      }
-      
-      auto end() const {
-        return std::find_if(states.cbegin(), states.cend(), [](auto a){return a == boost::none;});
-      }
-      
-      LightState getState() const {
-          
-      }
-    };
+
+enum class LightStates {
+  on, off, flicker
+};
+
+struct LightState {
+  int light;
+  LightStates state;
+};
+
+template <int lightNum, int stateNum> class LightsStates {
+  using LightsState = std::array<bool, lightNum>;
+  using TimedState = std::tuple<std::chrono::nanoseconds, LightsState>;
+  std::array<boost::optional<TimedState>, stateNum> states;
+
+ public:
+  auto& update(std::chrono::nanoseconds timePassed, const LightsState& state) {
+    auto& lastState = std::get<0>(states);
+
+    if (!lastState || std::get<1>(*lastState) != state) {
+      std::rotate(states.begin(), states.end() - 1, states.end());
+      std::get<0>(states) = std::make_tuple(timePassed, state);
+    } else {
+      auto& lastTime = std::get<0>(*lastState);
+      lastTime = timePassed + lastTime;
+    }
+
+    return *this;
+  }
+
+  auto begin() const { return states.cbegin(); }
+
+  auto end() const {
+    return std::find_if(states.cbegin(), states.cend(), [](auto a) {
+      return a == boost::none;
+    });
+  }
+
+  LightState getState() const {}
+};
+
 }
-
-
--- src/app.cpp
+++ src/app.cpp
@@ -3,21 +3,31 @@
 #include <iostream>
 
 using namespace std::chrono;
+using namespace arduinoTimer;
 using namespace std;
 
 int main(int argc, char** argv) {
+  LightsStates<3, 10> states;
+  states.update(0s, {
+    false, false, true
+  });
+  states.update(1s, {
+    false, false, true
+  });
+  states.update(2s, {
+    false, false, true
+  });
+  states.update(3s, {
+    false, false, true
+  });
+  states.update(4s, {
+    false, false, false
+  });
 
-    LightsStates<3,10> states;
-    states.update(0s, {false,false,true});
-    states.update(1s, {false,false,true});
-    states.update(2s, {false,false,true});
-    states.update(3s, {false,false,true});
-    states.update(4s, {false,false,false});
-    
-    for(const auto& x:states) {
-      auto timeStamp = duration_cast<seconds>(std::get<0>(*x));
-      cout << timeStamp.count() << endl;
-    }
+  for (const auto& x : states) {
+    auto timeStamp = duration_cast<seconds>(std::get<0>(*x));
+    cout << timeStamp.count() << endl;
+  }
 
-    return 0;
+  return 0;
 }
